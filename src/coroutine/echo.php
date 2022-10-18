568ef183f1509e8affecfecb1d2c54f793c993d3
Dmytro Sokil <dmytro.sokil@gmail.com>
2022-10-18 23:11:16 +0300

    coroutine

--- src/coroutine/echo.php
+++ src/coroutine/echo.php
@@ -1,28 +1,38 @@
 <?php
 
-function createEchoCoroutine(callable $callback): \Generator
+function createEchoCoroutine(): \Generator
 {
-    echo "Before callback" . PHP_EOL;
-    $callback(yield);
-    echo "After callback" . PHP_EOL;
+    echo "Before first yield" . PHP_EOL;
+    echo yield . PHP_EOL;
+    echo "After first yield" . PHP_EOL;
+    echo "Before second yield" . PHP_EOL;
+    echo yield . PHP_EOL;
+    echo "After second yield" . PHP_EOL;
 }
+
 echo "Before coroutine create" . PHP_EOL;
-$coroutine = createEchoCoroutine(
-    function (string $response) {
-        echo "Received response: " . $response . PHP_EOL;
-    }
-);
+$coroutine = createEchoCoroutine();
 echo "After coroutine create" . PHP_EOL;
-echo "Before send" . PHP_EOL;
+
+echo "Before send hello" . PHP_EOL;
 $coroutine->send('hello');
-echo "After send";
+echo "After send hello" . PHP_EOL;
+
+echo "Before send world" . PHP_EOL;
+$coroutine->send('world');
+echo "After send world";
 
 /**
 Before coroutine create
 After coroutine create
-Before send
-Before callback
-Received response: hello
-After callback
-After send
+Before send hello
+Before first yield
+hello
+After first yield
+Before second yield
+After send hello
+Before send world
+world
+After second yield
+After send world
  */
